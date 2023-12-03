139b7abdb75daf8dd4b9bc5cb28da67a2dd04ce4
Rishabh Kushwaha <rishabhkushwaha08@gmail.com>
2023-12-04 03:09:53 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useContext, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { QuizContext } from './context/Quiz'
+import Question from './components/Question'
+
+function App() {
+
+  const [quizState, dispatch] = useContext(QuizContext);
+  console.log(quizState);
+  return (
+    <div className='quiz'>
+
+    {quizState.showResults && (
+
+      <div className='results'>
+        <div className="completed">Quiz is completed</div>
+        <div className="results-info">
+          <div>You have completed the quiz</div>
+          <div>
+            Score : {quizState.correctAnswersCount} of {" "} {quizState.questions.length} right
+          </div>
+          <br />
+          <div className="next-button" onClick = {() => dispatch({type : "RESTART"})}>Restart quiz</div>
+        </div>
+      </div>
+      )
+    }
+
+      {!quizState.showResults && (
+        <div>
+          <div className='score'>
+            Question {quizState.currentQuestionIndex + 1}/{quizState.questions.length}
+          </div>
+          <Question />
+          <div className="next-button" onClick={() => dispatch({ type: "NEXT_QUE" })}>
+            {quizState.currentQuestionIndex === 7 ? "Finish Quiz" : "Next Question"}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
