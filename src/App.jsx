dc2e447fddb9035854171f2b90754a7af533d6cc
Rishabh Kushwaha <rishabhkushwaha08@gmail.com>
2023-12-04 03:12:01 +0530

    naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { QuizContext } from './context/Quiz'
 import Question from './components/Question'
